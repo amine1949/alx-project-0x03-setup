a5fabaf88a5b57532c054bb8202251483f98012c
amine1949 <amine.barik.2001@gmail.com>
2025-06-09 12:43:12 +0100

    Implementing a share layout

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <div className="pt-24"> {/* Add padding to account for fixed header */}
+        <h1 className="text-4xl font-bold text-center">Welcome to Splash App</h1>
+        {/* Your page content here */}
+      </div>
+  )
+}
